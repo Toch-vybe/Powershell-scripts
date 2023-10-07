b376fba268aa215cd4c81c3a2de09acb5365083e
Tochukwu Ohabuike <60288017+Toch-vybe@users.noreply.github.com>
2023-10-07 19:01:43 +0100

    Update vm-manager.ps1

--- vm-manager.ps1
+++ vm-manager.ps1
@@ -1,4 +1,4 @@
-Write-Host "`nWelcome, this script allows you to start, stop and check status of multiple VMs in various resource groups in parallel. `n" 
+Write-Host "`nWelcome, this script allows you to start, stop and check status of multiple VMs in various resource groups in parallel." 
 
 Write-Host "`nEnter action (start/stop/status):"
 
@@ -8,9 +8,10 @@ $VirtualMachinesDetails = Import-Csv .\VMLists.csv #Imports csv file from the sp
 
 if ($Action -eq "start"){
 
-    $OS_TYPE = "Windows"
+    Write-Host "Please specifiy the OS (Windows/Linux)"
+    $OS_TYPE = Read-Host
 
-    Write-Host "Please specifiy the environment (Prod/Dev/Test)"
+    Write-Host "Please specifiy the environment (prod/dev)"
 
     $VM_Environment = Read-Host
 
@@ -18,22 +19,23 @@ if ($Action -eq "start"){
 
     $VirtualMachinesDetails | Where-Object {($_.VM_OS -eq $OS_TYPE) -and ($_.VM_Environment -eq $VM_Environment)} | ForEach-Object -parallel { #filters and fetches the csv files content based on the OS type.
 
-        $checkIfVMExistInRG = get-azvm -ResourceGroupName $_.VM_RG -name $_.VM_Name -ErrorVariable notPresent -ErrorAction SilentlyContinue # Checks if the VM exists in the RG
+        $checkIfVMExistInRG = get-azvm -ResourceGroupName $_.ResourceGroupName -name $_.VM_Name -ErrorVariable notPresent -ErrorAction SilentlyContinue # Checks if the VM exists in the RG
 
         if ($notPresent){
             continue
         }
         else {
-            Start-AzVM -ResourceGroupName $_.VM_RG -Name $_.VM_Name
+            Start-AzVM -ResourceGroupName $_.ResourceGroupName -Name $_.VM_Name
         }
     } -ThrottleLimit 10
 
 }
 elseif ($Action -eq "stop"){
 
-    $OS_TYPE = "Windows"
+    Write-Host "Please specifiy the OS (Windows/Linux)"
+    $OS_TYPE = Read-Host
 
-    Write-Host "Please specifiy the environment (Prod/Dev/Test):"
+    Write-Host "Please specifiy the environment (prod/dev):"
 
     $VM_Environment = Read-Host
 
@@ -41,41 +43,60 @@ elseif ($Action -eq "stop"){
 
     $VirtualMachinesDetails | Where-Object {($_.VM_OS -eq $OS_TYPE) -and ($_.VM_Environment -eq $VM_Environment)} | ForEach-Object -parallel {
 
-        $checkIfVMExistInRG = get-azvm -ResourceGroupName $_.VM_RG -name $_.VM_Name -ErrorVariable notPresent -ErrorAction SilentlyContinue
+        $checkIfVMExistInRG = get-azvm -ResourceGroupName $_.ResourceGroupName -name $_.VM_Name -ErrorVariable notPresent -ErrorAction SilentlyContinue
 
         if ($notPresent){
             continue
         }
         else {
-            Stop-AzVM -ResourceGroupName $_.VM_RG -Name $_.VM_Name -Confirm:$false -ErrorAction SilentlyContinue -Force
+            Stop-AzVM -ResourceGroupName $_.ResourceGroupName -Name $_.VM_Name -Confirm:$false -ErrorAction SilentlyContinue -Force
         }
     } -ThrottleLimit 10
 }
 
 elseif($Action -eq "status"){
 
+    Write-Host "Please specifiy the OS (windows/linux/all)"
     $OS_TYPE = "Windows"
 
-    Write-Host "Please specifiy the environment (Prod/Dev/Test)"
-
-    $VM_Environment = Read-Host
+    Write-Host "Please specifiy the environment (prod/dev/all)"
+    $VM_Environment = "dev"
 
     Write-Host "`nGetting status of all $OS_TYPE Virtual machines in the $VM_Environment environment..."
 
     $Rg_Exist = @()
+    $Vm_Exist = @()
+    $vmStatuses = @()
+    
     $VirtualMachinesDetails | Where-Object {($_.VM_OS -eq $OS_TYPE) -and ($_.VM_Environment -eq $VM_Environment)} | ForEach-Object {
-      
-        if ($Rg_Exist.Contains($_.VM_RG)) {
+        if ($Rg_Exist.Contains($_.ResourceGroupName) -and $Vm_Exist.Contains($_.VM_Name)) {
             return
         }
         else {
-            Get-AzVM -ResourceGroupName $_.VM_RG -Status
-            $Rg_Exist += $_.VM_RG
+            
+            $VMstatus = Get-AzVM -Name $_.VM_Name -ResourceGroupName $_.ResourceGroupName -Status
+            $VMdetails = Get-AzVM -Name $_.VM_Name -ResourceGroupName $_.ResourceGroupName
+
+            $vmInfo = [PSCustomObject]@{
+                ResourceGroupName   = $vmStatus.ResourceGroupName
+                Name                = $vmStatus.Name
+                Location            = $VMdetails.Location
+                VmSize              = $VMdetails.HardwareProfile.VmSize
+                OsType              = $VMdetails.StorageProfile.OsDisk.OsType
+                Disk_Status         = $vmStatus.Disks.Statuses.code
+                VMAgentStatus       = $vmStatus.VMAgent.Statuses.DisplayStatus
+                PowerState          = $vmStatus.Statuses[1].DisplayStatus     
+                Provisioning        = $vmStatus.Statuses[0].DisplayStatus
+                MaintenanceAllowed  = $vmStatus.MaintenanceRedeployStatus.IsCustomerInitiatedMaintenanceAllowed
+            }
+            $vmStatuses += $vmInfo
+
+            $Rg_Exist += $_.ResourceGroupName
+            $Vm_Exist += $_.VM_Name
         }        
     }
-
-
- }
+    $vmStatuses | Format-Table -AutoSize
+}
 else {
     Write-Host "action not recognized"
 }
